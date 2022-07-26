ca9147d37d5cc562c97222e3e702ff1afbd58022
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-26 17:12:37 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import AppHeader from './components/header/AppHeader.vue'
+import AppHeader from "./components/header/AppHeader.vue";
 
 export default {
   name: "App",
@@ -16,7 +16,10 @@ export default {
 </script>
 
 <style lang="scss">
-*{
+@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css');
+
+* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
--- src/components/header/HeaderNavbar.vue
+++ src/components/header/HeaderNavbar.vue
@@ -1,15 +1,84 @@
 <template>
   <nav>
-
+    <div class="logo">
+      <img src="../../assets/img/theme_eduprime_logo.png" alt="" />
+    </div>
+    <div class="right-nav">
+      <ul>
+        <li v-for="(item, index) in liItems" :key="index">
+          {{ item }} <i class="fa-solid fa-caret-down"></i>
+        </li>
+      </ul>
+      <button>VIEW COURSES</button>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-
-}
+  data: function () {
+    return {
+      liItems: [
+        `Home`,
+        `Courses`,
+        `About Us`,
+        `News`,
+        `Pages`,
+        `Contact`,
+        `Purchase`,
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+* {
+  font-family: "Fredoka One", cursive;
+}
 
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 150px;
+  padding: 0 3rem;
+  color: #fff;
+}
+
+img {
+  height: 50px;
+}
+
+.right-nav {
+  display: flex;
+  align-items: center;
+}
+
+ul {
+  display: flex;
+}
+
+li {
+  list-style: none;
+  margin-left: 3rem;
+  &:hover {
+    color: #e9d758;
+    cursor: pointer;
+  }
+}
+
+button {
+  margin-left: 3rem;
+  border: none;
+  border-radius: 40px;
+  padding: 1.2rem 3rem;
+  color: #fff;
+  background-color: #e9d758;
+  font-size: 1.2rem;
+  &:hover{
+    cursor: pointer;
+    box-shadow: inset 0 0 10px;
+  }
+}
 </style>
