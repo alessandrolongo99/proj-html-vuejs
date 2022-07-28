90823cf8ae873bb8f3cf399ea3172625866528a8
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-28 09:56:13 +0200

    MainLatest: finished

--- src/components/main/sections/MainLatest.vue
+++ src/components/main/sections/MainLatest.vue
@@ -72,6 +72,42 @@
         <button><i class="fa-regular fa-eye"></i> View Course</button>
       </div>
     </div>
+
+    <div class="flex-container">
+      <div class="text-container">
+        <img class="height-70" src="../../../assets/img/Exam-icon.png" alt="" />
+        <h1>The most efficient examination method</h1>
+        <p>
+          EduPrime has gathered teachers from around the globe to brainstorm in
+          order to facilitate the evaluation of our students. Every teacher from
+          our university has an influence on how students are evaluated at
+          his/her subject.
+        </p>
+        <button>Discover the Method</button>
+      </div>
+
+      <div class="img-container">
+        <img src="../../../assets/img/Exam-Illustration.png" alt="" />
+      </div>
+    </div>
+
+    <div class="flex-container bot-row">
+      <div class="img-container">
+        <img src="../../../assets/img/Girl-Illustration.png" alt="" />
+      </div>
+
+      <div class="text-container">
+        <img class="height-70" src="../../../assets/img/Exam-icon-1.png" alt="" />
+        <h1>Variable fees for international students</h1>
+        <p>
+          EduPrime has gathered teachers from around the globe to brainstorm in
+          order to facilitate the evaluation of our students. Every teacher from
+          our university has an influence on how students are evaluated at
+          his/her subject.
+        </p>
+        <button>List of Fees</button>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -91,13 +127,65 @@ h1 {
   color: #454545;
   font-size: 3.2rem;
   margin-top: 1rem;
-  margin-bottom: 5rem;
+  margin-bottom: 2rem;
+}
+
+p {
+  font-family: "Roboto", sans-serif;
+  color: #8b8b8a;
+  margin-top: 2rem;
+  font-size: 1.05rem;
+  line-height: 1.8rem;
+}
+
+button {
+  font-family: "Fredoka One", cursive;
+  margin-top: 2rem;
+  border: none;
+  border-radius: 40px;
+  padding: 1.2rem 3rem;
+  color: #fff;
+  background-color: #e56768;
+  font-size: 1.2rem;
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    transition-property: box-shadow;
+    transition-duration: 0.5s;
+  }
+}
+
+.flex-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 9rem;
+}
+
+.bot-row{
+    margin-bottom: 4rem;
+}
+
+.height-70 {
+  height: 70px;
+}
+
+.text-container {
+  width: 50%;
+}
+
+.img-container {
+  width: 40%;
+  img {
+    width: 100%;
+  }
 }
 
 .latest {
   display: flex;
   justify-content: space-between;
   width: 100%;
+  margin-bottom: 6rem;
   .card {
     padding: 0 1rem;
     display: flex;
@@ -141,7 +229,7 @@ h1 {
       color: #fff;
       background-color: #e56768;
       box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
-      font-size: .8rem;
+      font-size: 0.8rem;
       &:hover {
         cursor: pointer;
         box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
